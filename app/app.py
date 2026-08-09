e1c454df2e9fc28c96aff9ac54df96e45712848e
George Saito <99691302+georges1138@users.noreply.github.com>
2026-08-08 21:12:35 -0400

    Extract create_app factory to allow test configuration

--- app/app.py
+++ app/app.py
@@ -7,31 +7,36 @@ from middlewares.authentication_middleware import require_login_middleware
 from models import db
 from config import Config
 
-app = Flask(__name__)
 
-# Load config
-app.config.from_object(Config)
+def create_app(test_config=None):
+    app = Flask(__name__)
 
-# Set a secret key for session management
-app.secret_key = 'your_unique_secret_key'
+    # Load config
+    app.config.from_object(Config)
 
-# Initialize the database
-db.init_app(app)
+    if test_config:
+        app.config.update(test_config)
 
-# Create database tables
-with app.app_context():
-    db.create_all()
+    # Initialize the database
+    db.init_app(app)
 
-# Setup error handler middleware
-setup_error_handler(app)
+    # Setup error handler middleware
+    setup_error_handler(app)
 
-# Setup authentication middleware
-require_login_middleware(app)
+    # Setup authentication middleware
+    require_login_middleware(app)
 
-# Register blueprints
-app.register_blueprint(todo_controller)
-app.register_blueprint(user_controller)
-app.register_blueprint(theme_controller)
+    # Register blueprints
+    app.register_blueprint(todo_controller)
+    app.register_blueprint(user_controller)
+    app.register_blueprint(theme_controller)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(host='0.0.0.0', port=3000, debug=True)
--- app/config.py
+++ app/config.py
@@ -3,5 +3,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.db')
+    SECRET_KEY = 'your_unique_secret_key'
+
+    SQLALCHEMY_DATABASE_URI = (
+            'sqlite:///' + os.path.join(basedir, 'data.db')
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
